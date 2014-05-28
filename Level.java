ba14a1ec33d219b0265452a147e764133bc5f25b
Rebecca Yuste-Golob <remiyg@gmail.com>
2014-05-28 13:31:14 -0400

    made a level class

---
+++ Level.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+/*purpose of this class:
+  like the "node" in the tree*/
+
+
+public class Level {
+    
+    private String name; 
+    private int number;
+
+    public Level (String s, int n){
+	name = s;
+	number = n;
+    }
+
+}
