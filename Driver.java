8ab3fc9b430023d571302865d5876940a7b2dc41
Rebecca Yuste-Golob <remiyg@gmail.com>
2014-05-29 13:40:17 -0400

    added a super basic driver

---
+++ Driver.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+    public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	//choose a character?
+	
+    }
+
+}
