f4ec8d8bc1ba5edca3a8bac0392e388e7884f889
Rebecca Yuste-Golob <remiyg@gmail.com>
2014-05-29 13:58:14 -0400

    READ FIRST, ideas and things

--- Player.java
+++ Player.java
@@ -6,5 +6,9 @@ public class Player {
     Level level;
     int health;
 
+    //Queue of items, sorry i wasn't able to do much in class, I was in the middle of a really exciting part in my book
+    //I'll work on it more at home tonight (i promise!)
+    //I was thinking maybe having the driver create a player based on the things it reads in
+    // we should talk about moving from one level to another
 
 }
