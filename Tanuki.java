ccae5f1e8c7c907be49dfe59ac96ad872e55a443
Jenny Baran <jenny.baran@gmail.com>
2014-06-03 14:42:00 -0400

    unfinished Tanuki skeleton

---
+++ Tanuki.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Tanuki {
+
+    String _question;
+    String _answer;
+
+    public boolean isCorrect( String ans ) {
+	return ans = _answer;
+    }
+
+}
