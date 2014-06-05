55cfa08ea2844f8359fa828e72abadc0105491a5
Jenny Baran <jenny.baran@gmail.com>
2014-06-05 16:59:47 -0400

    prepped player for item, completed item class

--- Item.java
+++ Item.java
@@ -2,15 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Item {
-    int health;
-    String name;
+    String _name;
+    String _response;
+    int _healthEffect;
 
-    public Item(){
-	health = 0;
-	name = "generic name";
+    public Item( String filename ) {
+	String[] splitString = new String[3]; //change if more data
+	int count = 0;
+	try {
+	    Scanner sc = new Scanner( new File( filename ) );
+	    while ( sc.hasNext() ) {
+		splitString[count] = sc.nextLine();
+		count++;
+	    }
+
+	    _name = splitString[0];
+	    _response = splitString[1];
+	    _healthEffect = Integer.parseInt( splitString[2] );
+	}
+	catch (Exception e) {
+	    System.out.println("File not found, you are a failure and should feel bad.");
+	    _name = "TINY CAR.";
+	    _response = "THE CAR WAS FULL OF CAMELS, YOU ARE DAZED AND CONFUSED.";
+	    _healthEffect = 0;
+	}
     }
 
-    public String toString(){
-	return name;
+    public String getName(){
+	return _name;
+    }
+
+    public String getResponse() {
+	return _response;
+    }
+
+    public int getEffect() {
+	return _healthEffect;
+    }
+
+    public static void main( String[] args ) {
+	Item tinyCar = new Item( "tinycar.txt" );
+	System.out.println( tinyCar.getName() );
+	System.out.println( tinyCar.getResponse() );
+	System.out.println( tinyCar.getEffect() );
     }
 }
--- Player.java
+++ Player.java
@@ -9,14 +9,8 @@ public class Player {
     int _mochi;
 
     public Player( Level lvl ) {
-	_name = "Kia"; //shoutout to the bae
+	_name = "Kia";
 	_level = lvl;
-	/**********
-	 * when player is created in board/driver/wherever,
-	 * lvl is the root of the board tree (additionally,
-	 * this makes it easy if we ever want to test the game
-	 * from the middle).
-	 **********/
 	_health = 100;
 	_iQ = new ItemQueue();
     }
@@ -31,40 +25,29 @@ public class Player {
 	_level = lvl;
     }
 
-    /*********
-     * Queue of items, sorry i wasn't able to do much in class,
-     * I was in the middle of a really exciting part in my book
-     * I'll work on it more at home tonight (i promise!)
-     * I was thinking maybe having the driver create a player based
-     * on the things it reads in
-     * we should talk about moving from one level to another
-     *********/
-
-    //don't worry about it! elaborate on queue of items?
-
     public void obtainItem(String s){
 	_iQ.enqueue(s);
     }
+
     public String peekItem() {
-	return _iQ.peekFront();
+	Item accessed = _iQ.peekFront();
+	return accessed.getName();
     }
 
     public String useItem() {
-	return _iQ.dequeue();
+	Item accessed = iQ.dequeue();
+	String retStr = accessed.getResponse();
+	_health += accessed.getEffect();
     }
 
     public void obtainMochi( int i ) {
 	//you get mochi randomly at the start of a level
 	//and also if you correctly answer a tanuki's question
+	//uses same randint as tanuki's mochi tax
 	_mochi += i;
     }
         
     public void useMochi( int i ) {
-	/*********
-	 * throwing mochi at a tanuki calls this with
-	 * i = 1 (so you throw mochi at the tanuki one
-	 * at a time and hope it goes away)
-	 *********/
 	_mochi -= i;
     }
 
