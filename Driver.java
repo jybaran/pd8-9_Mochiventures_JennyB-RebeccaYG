80729c18ef32179f04f1ece5574a55ed8664bdb4
Jenny Baran <jenny.baran@gmail.com>
2014-06-06 14:24:16 -0400

    poking at the driver

--- Driver.java
+++ Driver.java
@@ -3,43 +3,53 @@ import java.util.*;
 
 public class Driver {
 
-    Tanuki opponent;
+    Tanuki _opponent;
 
     public static void main (String[] args) {
-	/*Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 
 	System.out.println("Hello! Welcome to Mochiventures");
 	System.out.println("What is your name?");
-	String s = sc.next();
+	String name = sc.next();
 	
-	Player p = new Player(s);
-	Board b = new Board();*/
+	Player _player = new Player(name);
+	Board _board = new Board();
 
-	try{
-	    Scanner sc = new Scanner(new File("Level1.txt"));
-	    while ( sc.hasNext() )
-		System.out.println(sc.nextLine());
-	} catch (Exception e){
-	    System.out.println("oops file not found");
+	String[] lvlNames = new String[]; //ah shit gotta make it a queue
+	int count = 0;
+
+	//gotta make this read in file of filenames!
+	try {
+	    Scanner sc = new Scanner( new File("LevelNames.txt") );
+	    while ( sc.hasNext() ) {
+		lvlNames[count] = sc.nextLine() + ".txt";
+	    }
+	}
+	catch (Exception e) {
+	    System.out.println("FILE NOT FOUND, YOU ARE A FAILURE AND SHOULD FEEL BAD.");
 	}
 
-	//when leveling up, player.setlevel( board.lvlupr/l() ) 
+	for ( String lvlnm : lvlNames ) {
+	    _board.add( new Level(lvlnm) );
+	}
 
-	//if ( player.getlevel().hastanuki() )
-	//    opponent = player.getlevel().gettanuki()
+	//when leveling up, _player.setlevel( board.lvlupr/l() ) 
+
+	//if ( _player.getlevel().hastanuki() )
+	//    _opponent = _player.getlevel().gettanuki()
 	//    SOP you ran into a tanuki, what do you do
 	//    choices:
 	//        run (damages yr health)
-	//            player.adjustHealth( opponent.getHEffect() )
-	//        throw opponent.getMTax() mochi
-	//            player.useMochi( opponent.getMTax() )
+	//            _player.adjustHealth( _opponent.getHEffect() )
+	//        throw _opponent.getMTax() mochi
+	//            _player.useMochi( _opponent.getMTax() )
 	//        answer question
-	//            SOP opponent.getDescription()
+	//            SOP _opponent.getDescription()
 	//            user input string
-	//            if opponent.iscorrect(input)
-	//                player.obtainMochi( opponent.getMTax )
+	//            if _opponent.iscorrect(input)
+	//                _player.obtainMochi( _opponent.getMTax )
 	//            else
-	//                player.useMochi( opponent.getMTax )
+	//                _player.useMochi( _opponent.getMTax )
 	//    level up l/r?
 	
     }
